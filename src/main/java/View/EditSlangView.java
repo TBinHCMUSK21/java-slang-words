1275e19298a09801cf3397cb891d5a691e264fcc
TBin <TBinTB0003@gmail.com>
2023-11-06 23:50:44 +0700

    Add the detail comment of the EditSlangView.java

--- src/main/java/View/EditSlangView.java
+++ src/main/java/View/EditSlangView.java
@@ -14,9 +14,9 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class EditSlangView extends JFrame {
-    private SlideBarView sidebar;
-    private DefaultListModel<String> listModel;
-    private JTextField slangField;
+    /**
+     * Main frame
+     */
     public EditSlangView() {
         setTitle("Slang Dictionary Search");
         initializeComponents();
@@ -26,62 +26,91 @@ public class EditSlangView extends JFrame {
         setVisible(true);
     }
 
+    /**
+     * Add the sidebar
+     */
     private void initializeComponents() {
         Font mainFont = new Font("Arial", Font.PLAIN, 18);
 
         setLayout(new BorderLayout());
-        sidebar = new SlideBarView();
+        SlideBarView sidebar = new SlideBarView();
         add(sidebar, BorderLayout.WEST);
         add(createMainContent(mainFont), BorderLayout.CENTER);
     }
+
+    /**
+     * Create a main content of frame
+     * @param font: Font of the content
+     * @return mainContent: a JPanel of the main content
+     */
     private JPanel createMainContent(Font font) {
         JPanel mainContent = new JPanel(new BorderLayout());
         mainContent.setBackground(Color.WHITE);
         mainContent.add(createTitle(), BorderLayout.NORTH);
-        mainContent.add(createAddPanel(font), BorderLayout.CENTER);
+        mainContent.add(createEditPanel(font), BorderLayout.CENTER);
         mainContent.add(createSearchPanel(), BorderLayout.SOUTH);
 
         return mainContent;
     }
+
+    /**
+     * Create a title of the main content
+     * @return mainTitle: The main title of the main content
+     */
     private JLabel createTitle() {
         JLabel mainTitle = new JLabel("Edit slang word", SwingConstants.CENTER);
         mainTitle.setFont(new Font("Arial", Font.BOLD, 30));
         mainTitle.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
         return mainTitle;
     }
-    private JPanel createAddPanel(Font font) {
-        JPanel searchPanel = new JPanel();
-        searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.PAGE_AXIS));
-        searchPanel.add(createInputPanel(font), BorderLayout.NORTH);
-        return searchPanel;
+
+    /**
+     * Create the Edit panel
+     * @param font: font of the main content
+     * @return editPanel: the JPanel edit panel
+     */
+    private JPanel createEditPanel(Font font) {
+        JPanel editPanel = new JPanel();
+        editPanel.setLayout(new BoxLayout(editPanel, BoxLayout.PAGE_AXIS));
+        editPanel.add(createInputPanel(font), BorderLayout.NORTH);
+        return editPanel;
     }
+
+    /**
+     * The input panel to get the user input
+     * @param font: font of the main content
+     * @return inputPanel: The JPanel to input the slang word
+     */
     private JPanel createInputPanel(Font font) {
         JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new GridBagLayout());
         inputPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
 
+        // Create Layout
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.fill = GridBagConstraints.BOTH;
         gbc.insets = new Insets(5, 5, 5, 5);
 
+        // Add the title
         JLabel slangLabel = new JLabel("Slang Word:");
         slangLabel.setFont(font);
         gbc.weightx = 0.1;
         inputPanel.add(slangLabel, gbc);
 
-        slangField = new JTextField(15);
+        // Add the field to input
+        JTextField slangField = new JTextField(15);
         slangField.setFont(font);
         gbc.weightx = 1.0;
         inputPanel.add(slangField, gbc);
 
-
+        // Add the button
         JButton findButton = createButton("Find", font);
         gbc.weightx = 0.0;
 
         inputPanel.add(findButton, gbc);
 
-
+        // Deal with the space empty
         GridBagConstraints gbcFiller = new GridBagConstraints();
         gbcFiller.gridwidth = GridBagConstraints.REMAINDER;
         gbcFiller.fill = GridBagConstraints.BOTH;
@@ -91,49 +120,61 @@ public class EditSlangView extends JFrame {
 
         return inputPanel;
     }
-    private JButton createButton(String title, Font font, ActionListener action) {
+
+    /**
+     * Create a button
+     * @param title: A button title
+     * @param font: A font of the main content
+     * @return button: A JButton button
+     */
+    private JButton createButton(String title, Font font) {
         JButton button = new JButton(title);
         button.setFont(font);
-        button.addActionListener(action);
         return button;
     }
 
-    private JButton createButton(String title, Font font) {
-        return createButton(title, font, null);
-    }
+    /**
+     * A panel to display a list of definition
+     * @return searchPanel: A JPanel to display
+     */
     private JPanel createSearchPanel() {
         JPanel searchPanel = new JPanel();
         searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.PAGE_AXIS));
         searchPanel.add(createListScroller(), BorderLayout.CENTER);
         return searchPanel;
     }
+
+    /**
+     * Create a table to display a list slang-definition
+     * @return scrollPane: A JScrollPane to display a list slang-definitions
+     */
     private JScrollPane createListScroller() {
         DefaultTableModel tableModel = new DefaultTableModel(new Object[]{"STT", "Slang", "Meaning"}, 0) {};
 
-        // Tạo bảng với model đã tạo
+        // Create Table
         Font tableFont = new Font("Arial", Font.PLAIN, 16);
         JTable table = new JTable(tableModel);
         table.setFont(tableFont);
         JTableHeader header = table.getTableHeader();
         header.setFont(tableFont);
 
-        // Thêm dữ liệu vào bảng
+        // Add data to table
         tableModel.addRow(new Object[]{1, "Slang 1", "Meaning 1"});
         tableModel.addRow(new Object[]{2, "Slang 2", "Meaning 2"});
         tableModel.addRow(new Object[]{3, "Slang 3", "Meaning 3"});
 
 
-        // Đặt các thuộc tính khác cho bảng nếu cần
+        // Feature for table
         table.setFillsViewportHeight(false);
         table.setRowHeight(30);
 
-        // Thêm bảng vào JScrollPane
+        // Add table to JScrollPane
         JScrollPane scrollPane = new JScrollPane(table);
         Dimension preferredSize = scrollPane.getPreferredSize();
         preferredSize.height = 370;
         scrollPane.setPreferredSize(preferredSize);
 
-        // Thiết lập cửa sổ
+        // Display window
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
